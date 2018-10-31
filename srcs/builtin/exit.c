dc1f670dbc75b6b6c888d226a7852de08c4d538f
William Braeckman <wbraeckm@student.s19.be>
2018-10-31 16:51:32 +0100

    Change to builtin exit

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/29 16:41:59 by wbraeckm          #+#    #+#             */
-/*   Updated: 2018/10/29 16:44:16 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2018/10/31 16:51:25 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int		exit_builtin(t_shell *shell, int argc, char **argv)
 {
 	if (argc == 1)
 	{
+		ft_printf("exit\n");
 		destroy_shell(shell);
 		exit(0);
 	}
@@ -24,7 +25,10 @@ int		exit_builtin(t_shell *shell, int argc, char **argv)
 		if (!ft_strisnumber(argv[1]))
 			ft_printf_fd(2, "exit: Badly formed number.\n");
 		else
+		{
+			ft_printf("exit\n");
 			exit((unsigned char)ft_atoi(argv[1]));
+		}
 	}
 	return (0);
 }
