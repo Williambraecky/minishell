ffbc8685ed5dcd85dcdd66cb4af8ca5322cb2f1f
William Braeckman <wbraeckm@student.s19.be>
2018-10-31 16:39:15 +0100

    update minishell

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/29 13:45:18 by wbraeckm          #+#    #+#             */
-/*   Updated: 2018/10/29 16:53:14 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2018/10/31 16:39:03 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,6 @@ int	main(int argc, char **argv, char **env)
 	if (!(shell = ft_memalloc(sizeof(*shell))))
 		error_exit("Out of memory");
 	init_env(shell, env);
-	char	buffer[512];
-	//cd_builtin(shell, 1, NULL);
-	getcwd(buffer, 512);
-	ft_printf("%s\n", buffer);
 	destroy_shell(shell);
 	return (0);
 }
