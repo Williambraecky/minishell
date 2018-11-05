8f34b48a101fd144c849c60fd96e95017ae9dc90
William Braeckman <wbraeckm@student.s19.be>
2018-11-05 23:58:20 +0100

    Norminette fix setenv

--- srcs/builtin/setenv.c
+++ srcs/builtin/setenv.c
@@ -6,41 +6,42 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/29 15:57:16 by wbraeckm          #+#    #+#             */
-/*   Updated: 2018/11/05 19:08:23 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2018/11/05 23:58:09 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	check_arg(char *arg)
+{
+	while (*arg)
+		if (!ft_isalnum(*arg++))
+			return (0);
+	return (1);
+}
+
 /*
 ** TODO: 27 lines
 */
 
-int		setenv_builtin(t_shell *shell, int argc, char **argv)
+int			setenv_builtin(t_shell *shell, int argc, char **argv)
 {
-	size_t	i;
-
 	if (argc == 1)
 	{
 		print_env(shell);
 		return (0);
 	}
 	else if (argc > 3)
-		ft_printf_fd(2, "setenv: Too many arguments.\n");
-	if (argc > 3)
-		return (0);
-	i = 0;
-	while (argc <= 3 && argv[1][i])
 	{
-		if (!ft_isalnum(argv[1][i]))
-		{
-			ft_printf_fd(2, "setenv: Variable name must contain \
-			alphanumeric characters.\n");
-			return (0);
-		}
-		i++;
+		ft_printf_fd(2, "setenv: Too many arguments.\n");
+		return (0);
 	}
-	if (get_env(shell, argv[1]) != NULL)
+	if (!ft_isalpha(argv[1][0]))
+		ft_printf_fd(2, "setenv: Variable name must begin with a letter.\n");
+	else if (!check_arg(argv[1]))
+		ft_printf_fd(2, "setenv: Variable name must contain \
+alphanumeric characters.\n");
+	else if (get_env(shell, argv[1]) != NULL)
 		repl_env(shell, argv[1], argc == 2 ? "" : argv[2]);
 	else
 		add_env(shell, argv[1], argc == 2 ? "" : argv[2]);
