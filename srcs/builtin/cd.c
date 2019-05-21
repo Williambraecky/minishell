d31048839f9e77d2ed5a3f665732bec8fe0d42d9
William Braeckman <wbraeckm@student.s19.be>
2019-05-21 14:16:35 +0200

    Fix cd not exiting with good return code and double free

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/29 16:44:30 by wbraeckm          #+#    #+#             */
-/*   Updated: 2018/11/28 17:47:59 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/05/21 14:14:22 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ static char	*handle_3_args(char *pwd, char **argv)
 
 	if (!ft_strstr(pwd, argv[1]))
 	{
-		free(pwd);
 		ft_printf_fd(2, "cd: string not in pwd: %s\n", argv[1]);
 		return (NULL);
 	}
@@ -31,16 +30,17 @@ static char	*handle_3_args(char *pwd, char **argv)
 	return (ret);
 }
 
-static void	exec_cd(t_shell *shell, char *old_pwd, char *path, int fr)
+static int	exec_cd(t_shell *shell, char *old_pwd, char *path, int fr)
 {
 	struct stat	st;
+	char		buff[4097];
 
 	if (path && stat(path, &st) == -1)
-		ft_printf_fd(2, "cd: no such file or directory: %s\n", path);
+		return (ft_printf_fd(2, "cd: no such file or directory: %s\n", path));
 	else if (path && !S_ISDIR(st.st_mode))
-		ft_printf_fd(2, "cd: not a directory: %s\n", path);
+		return (ft_printf_fd(2, "cd: not a directory: %s\n", path));
 	else if (path && access(path, R_OK) == -1)
-		ft_printf_fd(2, "cd: permission denied: %s\n", path);
+		return (ft_printf_fd(2, "cd: permission denied: %s\n", path));
 	else
 	{
 		if (get_env(shell, "OLDPWD"))
@@ -48,15 +48,21 @@ static void	exec_cd(t_shell *shell, char *old_pwd, char *path, int fr)
 		else
 			add_env(shell, "OLDPWD", old_pwd);
 		chdir(path);
+		if (get_env(shell, "PWD"))
+			repl_env(shell, "PWD", getcwd(buff, 4096));
+		else
+			add_env(shell, "PWD", getcwd(buff, 4096));
 	}
 	if (fr)
 		free(path);
+	return (0);
 }
 
 int			cd_builtin(t_shell *shell, int argc, char **argv)
 {
 	char		*old_pwd;
 	char		*path;
+	int			ret;
 
 	if (!(old_pwd = get_pwd()))
 		ft_printf_fd(2, "cd: Out of memory\n");
@@ -64,11 +70,8 @@ int			cd_builtin(t_shell *shell, int argc, char **argv)
 		path = get_env(shell, "HOME");
 	else if (argc == 2)
 	{
-		if (ft_strequ(argv[1], "-"))
-		{
-			if (!(path = get_env(shell, "OLDPWD")))
-				ft_printf_fd(2, "cd: no OLDPWD env set\n");
-		}
+		if (ft_strequ(argv[1], "-") && !(path = get_env(shell, "OLDPWD")))
+			ret = ft_printf_fd(2, "cd: no OLDPWD env set\n");
 		else
 			path = argv[1];
 	}
@@ -76,8 +79,9 @@ int			cd_builtin(t_shell *shell, int argc, char **argv)
 		path = handle_3_args(old_pwd, argv);
 	else if (argc > 3)
 		ft_printf_fd(2, "cd: too many arguments\n");
+	ret = 0;
 	if (old_pwd && path && argc <= 3)
-		exec_cd(shell, old_pwd, path, argc == 3);
+		ret = exec_cd(shell, old_pwd, path, argc == 3);
 	free(old_pwd);
-	return (0);
+	return (ret != 0);
 }
